6ee97045abc49edace989217fcbbd394e20d3687
Dig-Bick <1239343496@qq.com>
2023-04-10 21:58:22 +0800

    router

--- src/router/index.js
+++ src/router/index.js
@@ -99,5 +99,15 @@ const router = createRouter({
   history: createWebHistory(),
   routes,
 });
-
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    if (!localStorage.getItem("token")) {
+      next({ name: "Login" });
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+});
 export default router;
