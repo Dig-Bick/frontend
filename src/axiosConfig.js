cd50095c516bcc1b2e6a3aef4bda03adfa80e343
Dig-Bick <1239343496@qq.com>
2023-04-11 18:21:35 +0800

    Update axiosConfig.js

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const instance = axios.create();
+const instance = axios.create({
+  baseURL: 'http://localhost:8080'
+});
+
 
 instance.interceptors.request.use((config) => {
   config.headers.Authorization = "Bearer " + localStorage.getItem("token");
