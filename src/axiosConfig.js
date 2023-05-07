a00c7d0323ecd07c2f4e6fa9a07ede2d175b38f3
Dig-Bick <1239343496@qq.com>
2023-05-07 18:18:01 +0800

    暂时能发帖

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,13 +1,19 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8080'
+  baseURL: "http://localhost:8080",
 });
-
 
 instance.interceptors.request.use((config) => {
   config.headers.Authorization = "Bearer " + localStorage.getItem("token");
   return config;
 });
+
+instance.interceptors.response.use(
+  response => response, // Replace 'response => response.data' with this line
+  error => {
+    return Promise.reject(error);
+  }
+);
 
 export default instance;
